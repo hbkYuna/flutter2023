7c893393738fa6998d79cfb385b4158d9d5bd2c9
HbkYuna <strawhatnxahx@gmail.com>
2023-06-15 13:54:10 +0200

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_herexamen_noah2023
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_herexamen_noah2023/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_herexamen_noah2023
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
