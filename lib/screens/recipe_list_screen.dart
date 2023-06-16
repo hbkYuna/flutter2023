e14b37167f08517be47480a40487777d6670b8e8
HbkYuna <strawhatnxahx@gmail.com>
2023-06-16 14:29:41 +0200

    recipeListScreen static data until API config

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -8,9 +8,33 @@ class RecipeListScreen extends StatefulWidget {
 
 class _RecipeListScreenState extends State<RecipeListScreen> {
   List<Recipe> recipes = [
-    Recipe('Recipe 1', 'Description 1'),
-    Recipe('Recipe 2', 'Description 2'),
-    Recipe('Recipe 3', 'Description 3'),
+    Recipe(
+      id: '1',
+      name: 'Recipe 1',
+      category: 'Category 1',
+      area: 'Area 1',
+      instructions: 'Instructions 1',
+      thumbnailUrl: 'thumbnail_url_1',
+      ingredients: ['Ingredient 1', 'Ingredient 2'],
+    ),
+    Recipe(
+      id: '2',
+      name: 'Recipe 2',
+      category: 'Category 2',
+      area: 'Area 2',
+      instructions: 'Instructions 2',
+      thumbnailUrl: 'thumbnail_url_2',
+      ingredients: ['Ingredient 3', 'Ingredient 4'],
+    ),
+    Recipe(
+      id: '3',
+      name: 'Recipe 3',
+      category: 'Category 3',
+      area: 'Area 3',
+      instructions: 'Instructions 3',
+      thumbnailUrl: 'thumbnail_url_3',
+      ingredients: ['Ingredient 5', 'Ingredient 6'],
+    ),
   ];
 
   @override
@@ -23,7 +47,7 @@ class _RecipeListScreenState extends State<RecipeListScreen> {
         itemCount: recipes.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(recipes[index].title),
+            title: Text(recipes[index].name),
             subtitle: Text(recipes[index].description),
           );
         },
