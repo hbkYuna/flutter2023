8ac118f064a541199e606e697962112265507e2a
HbkYuna <strawhatnxahx@gmail.com>
2023-06-18 22:20:38 +0200

    RecipeDetails made scrollable (prevent Overflow)

--- lib/screens/RecipeDetailScreen.dart
+++ lib/screens/RecipeDetailScreen.dart
@@ -12,50 +12,42 @@ class RecipeDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Recipe Details'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(recipe.thumbnailUrl),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  recipe.name,
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  'Category: ${recipe.category}',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  'Area: ${recipe.area}',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Ingredients:',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Column(
-                  children: recipe.ingredients.map((ingredient) {
-                    return Text('- $ingredient');
-                  }).toList(),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Instructions:',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8.0),
-                Text(recipe.instructions),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(recipe.thumbnailUrl),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    recipe.name,
+                    style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Category: ${recipe.category}',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Area: ${recipe.area}',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Instructions:',
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(recipe.instructions),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
