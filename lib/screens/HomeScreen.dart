cfed4e91fa9a9c7d0bf39fe37fc9110d523e6788
HbkYuna <strawhatnxahx@gmail.com>
2023-06-18 22:20:47 +0200

    Homescreen UI changes

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -12,16 +12,20 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Welcome to the Recipe App!',
+              'Welcome to my Recipe App',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the recipe list screen
-                Navigator.pushNamed(context, '/recipe-list');
-              },
-              child: Text('View Recipes'),
+            Image.asset(
+              'lib/assets/images/recipe_app_logo.png', 
+              height: 200,
+              width: 200,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'In this app, you will be able to explore some randomized recipes each time you enter our Recipe screen and filter them to your liking.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
             ),
           ],
         ),
